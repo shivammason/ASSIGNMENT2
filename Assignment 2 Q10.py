7e009f4712eec1709931b5ec9fb3fd9742ad3324
Shivam Masaon <200426022@student.georgianc.on.ca>
2019-05-23 05:02:12 -0400

    Create Assignment 2 Q10.py

---
+++ Assignment 2 Q10.py
@@ -0,0 +1,26 @@
+
+def computegrade(score):
+if score >= 0.0 and score <= 1.0:
+    if score >= 0.9:
+            print('A')
+        elif score >= 0.8:
+            print('B')
+        elif score >= 0.7:
+            print('C')
+        elif score >= 0.6:
+            print('D')
+        else:
+            print('F')
+
+    else:
+        print('Bad score')
+
+
+input_score = input('Enter score: ')
+try:
+    score = float(input_score)
+except:
+    print('Bad score')
+    quit()
+
+computegrade(score)
