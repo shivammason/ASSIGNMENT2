b6548fbbaa2846bdf759cdade553d28499d5ae58
Shivam Masaon <200426022@student.georgianc.on.ca>
2019-05-23 03:11:45 -0400

    Create Assignment 2 Q4.py

---
+++ Assignment 2 Q4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+input_temp = input('Enter temperature in celsius: ')
+temp = float(input_temp)
+fah_temp = temp*(9/5) + 32
+
+print('Temperature in fah: ', fah_temp)
+
+
+# In[ ]:
+
+
+
+
