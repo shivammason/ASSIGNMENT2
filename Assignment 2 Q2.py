1779a4077d062f0061d7bff01a4d298fd3b1304c
Shivam Masaon <200426022@student.georgianc.on.ca>
2019-05-23 03:03:45 -0400

    Create Assignment 2 Q2.py

---
+++ Assignment 2 Q2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+input_hours = input('Enter hours: ')
+input_rate = input('Enter rate: ')
+
+hours = float(input_hours)
+rate = float(input_rate)
+
+output = hours * rate
+print('pay', output)
+
+
+# In[ ]:
+
+
+
+
